a8f7caf922e7e9e28b224dbe381d56ffaef8d79d
yongnicholaskim <yongnicholaskim@gmail.com>
2018-05-29 13:31:56 -0400

    create index.js

---
+++ age-of-code-frontend/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const URL = `http://localhost:3000`;
+
+  const mainContainer = document.getElementById("main-container");
+  const userWindow = document.getElementById("user-window");
+  const userName = document.getElementById("user-name");
+  const userData = document.getElementById("user-data");
+  const actionsContainer = document.getElementById("actions-container");
+  const actionsList = document.getElementById("actions-list");
+
+});
