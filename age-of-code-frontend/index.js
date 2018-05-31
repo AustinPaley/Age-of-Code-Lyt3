c87c4134a8999295db86e15c1630ccd198513abc
AustinPaley <austinpaley32@gmail.com>
2018-05-31 14:39:21 -0400

    Added Add Button Code

--- age-of-code-frontend/index.js
+++ age-of-code-frontend/index.js
@@ -66,27 +66,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
   //ACTION OBJECT PARSER
   function createActions(actionObj){
+    //buttons are being added to the user
     let userPermissions = permissions[0]
-    let button = document.createElement("BUTTON")
-    button.setAttribute("class", "action")
-
-    button.setAttribute("class", "action actionButton")
-    button.setAttribute("id", `skill${actionObj.id}`)
     if (userPermissions.charAt(actionObj.id - 1) == 1){
-      button.innerHTML = `<div class="actionBar">${actionObj.name}</div>`
-      buttons.appendChild(button)
-      let skill = document.getElementById(`skill${actionObj.id}`)
-      skill.addEventListener("click",() => skillLogic(val, skill, actionObj.value, actionObj.cooldown))
-      skill.addEventListener("click",() => postToScreen(actionObj.name))
+      createButton(actionObj.name, actionObj.id, actionObj.value, actionObj.cooldown)
     }
-    let button2 = document.createElement("BUTTON")
-    button2.setAttribute("class", "action")
-
-    button2.setAttribute("class", "action actionButton")
-    button2.setAttribute("id", `skill${actionObj.id}`)
-    if (userPermissions.charAt(actionObj.id - 1) != 1){
-      button2.innerHTML = `<div class="actionBar">${actionObj.name}</div>`
-      shop.appendChild(button2)
+    else{
+      // adds the button to a "button div" to make it prettier
+      let shopButton = document.createElement("div")
+      shopButton.setAttribute("class", "buttcontainer")
+      shopButton.innerHTML = ` <p class="header">${actionObj.name}</p>`
+      shopButton.addEventListener("click", () => {createButton(actionObj.name, actionObj.id, actionObj.value, actionObj.cooldown)} )
+      // add in
+      //<p class="flavor">${actionObj.flavor}</p>
+      //<p class="price">${actionObj.price}</p>
+      shop.appendChild(shopButton)
     }
   }
   //
@@ -280,3 +274,13 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 });
+
+function createButton(name, id, value, cd){
+  let button = document.createElement("BUTTON")
+  button.setAttribute("class", "action actionButton")
+  button.setAttribute("id", `skill${id}`)
+  button.innerHTML = `<div class="actionBar">${name}</div>`
+  button.addEventListener("click",() => skillLogic(val, button, value, cd))
+  button.addEventListener("click",() => postToScreen(name))
+  buttons.appendChild(button)
+}
